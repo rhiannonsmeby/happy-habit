49a140639bee0a39e4976d881fe89feda5d8b7b2
Rhiannon Smeby <smebyrhi@gmail.com>
2021-03-09 13:53:21 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Happy Habit
+      </header>
+    </div>
+  );
+}
+
+export default App;
